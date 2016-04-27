6a2e4d9fd7e956aa98876df2caf772dd377b8e60
Theo Gregory <tjmg@outlook.com>
2016-04-27 22:33:01 +0100

    Changes key binding. Adds broken GET code.

--- function.js
+++ function.js
@@ -1,10 +1,26 @@
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
-    var dummy = document.createElement("input");
-    document.body.appendChild(dummy);
-	dummy.setAttribute("id", "dummy_id");
-	document.getElementById("dummy_id").value=url;  
-	dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
+
+    requrl = "http://rod.gs/?longurl=" + url;
+
+    var xhr = new XMLHttpRequest();
+	xhr.open("GET", requrl, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			var shorturl = xhr.responseText;
+
+			var dummy = document.createElement("input");
+		    document.body.appendChild(dummy);
+			dummy.setAttribute("id", "dummy_id");
+			document.getElementById("dummy_id").value=shorturl;  
+			dummy.select();
+		    document.execCommand("copy");
+		    document.body.removeChild(dummy);
+
+		    alert("Copied: " + shorturl);
+
+		}
+		alert("not worked");
+	}
+	xhr.send();
 });
