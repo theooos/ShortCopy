469db52b875d4a6529acf7d3254d44af3d647b11
Theo Gregory <tjmg@outlook.com>
2016-04-28 19:05:29 +0100

    Code fully works! Now to make it all pretty.

--- background.js
+++ background.js
@@ -1,12 +1,44 @@
-chrome.commands.onCommand.addListener(function(command) {
+document.write('<scr'+'ipt src="https://apis.google.com/js/client.js"></scr'+'ipt>');
 
-	var dummy = document.createElement("input");
-    document.body.appendChild(dummy);
-	dummy.setAttribute("id", "dummy_id");
-	document.getElementById("dummy_id").value="blah";  
-	dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
+function getTab(callback){
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+	    callback(tabs[0].url);
+	});
+}
 
-    alert("Copied: " + "blah");
+function makeShort(longUrl){
+
+    var request = gapi.client.urlshortener.url.insert({
+    	'longUrl': longUrl
+    });
+
+    request.execute(function(response){
+		if(response.id != null){
+			var dummy = document.createElement("input");
+		    document.body.appendChild(dummy);
+			dummy.setAttribute("id", "dummy_id");
+			document.getElementById("dummy_id").value=response.id;  
+			dummy.select();
+			document.execCommand("copy");
+			document.body.removeChild(dummy);
+			alert("Copied: "+response.id+"!");
+		}
+		else{
+			alert("Error shortening " + longUrl + ". Please report.");
+		}
+	});
+}
+
+function load()	{
+	gapi.client.setApiKey('BLAH');
+	gapi.client.load('urlshortener', 'v1',function(){});
+}
+
+var millisecondsToWait = 500;
+setTimeout(function(){
+	load();
+}, millisecondsToWait);
+
+chrome.commands.onCommand.addListener(function(tab) {
+	getTab(makeShort);
 });
