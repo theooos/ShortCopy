d2f2f7f14619cc0163a0c10a21c840724e737ed8
Theo Gregory <tjmg@outlook.com>
2016-06-07 21:23:38 +0100

    Large re-write. Now uses is.gd due to ease.

--- background.js
+++ background.js
@@ -1,44 +1,43 @@
-document.write('<scr'+'ipt src="https://apis.google.com/js/client.js"></scr'+'ipt>');
-
 function getTab(callback){
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 	    callback(tabs[0].url);
 	});
 }
 
-function makeShort(longUrl){
+function makeShort(longURL){
+	console.log(longURL);
+    var xhr = new XMLHttpRequest();
 
-    var request = gapi.client.urlshortener.url.insert({
-    	'longUrl': longUrl
-    });
-
-    request.execute(function(response){
-		if(response.id != null){
-			var dummy = document.createElement("input");
-		    document.body.appendChild(dummy);
-			dummy.setAttribute("id", "dummy_id");
-			document.getElementById("dummy_id").value=response.id;  
-			dummy.select();
-			document.execCommand("copy");
-			document.body.removeChild(dummy);
-			alert("Copied: "+response.id+". Thanks for using ShortCopy.");
+    xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			var data = JSON.parse(xhr.responseText);
+			var url = data["shorturl"];
+			copyToClipboard(url);
 		}
-		else{
-			alert("Error shortening " + longUrl + ". Please report.");
-		}
-	});
-}
+    }
 
-function load()	{
-	gapi.client.setApiKey('INSERT ACCESS TOKEN HERE');
-	gapi.client.load('urlshortener', 'v1',function(){});
-}
+    var format = encodeURIComponent("json");
+    var address = encodeURIComponent(longURL);
 
-var millisecondsToWait = 500;
-setTimeout(function(){
-	load();
-}, millisecondsToWait);
+    var params = "format=" + format + "&url=" + address;
+    params = params.replace(/%20/g, '+');
+
+    xhr.open("GET","https://is.gd/create.php?"+params, true);
+    xhr.send();
+}
 
 chrome.commands.onCommand.addListener(function(tab) {
 	getTab(makeShort);
 });
+
+// Copies the response to the clipboard.
+function copyToClipboard(url){
+	var dummy = document.createElement("input");
+	document.body.appendChild(dummy);
+	dummy.setAttribute("id", "dummy_id");
+	document.getElementById("dummy_id").value=url;  
+	dummy.select();
+	document.execCommand("copy");
+	document.body.removeChild(dummy);
+	alert("Copied: "+url+". Thanks for using ShortCopy.");
+}
